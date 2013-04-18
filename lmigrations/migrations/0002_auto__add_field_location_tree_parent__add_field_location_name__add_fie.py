3c49ec01ee579aabe1601d8abf80b5930e0c71a0
Alfred Assey M <asseym@gmail.com>
2013-04-18 13:44:06 +0200

    initial commit

---
+++ lmigrations/migrations/0002_auto__add_field_location_tree_parent__add_field_location_name__add_fie.py
@@ -0,0 +1,124 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding field 'Location.tree_parent'
+        db.add_column('locations_location', 'tree_parent',
+                      self.gf('django.db.models.fields.related.ForeignKey')(blank=True, related_name='children', null=True, to=orm['locations.Location']),
+                      keep_default=False)
+
+        # Adding field 'Location.name'
+        db.add_column('locations_location', 'name',
+                      self.gf('django.db.models.fields.CharField')(default=1, max_length=100),
+                      keep_default=False)
+
+        # Adding field 'Location.status'
+        db.add_column('locations_location', 'status',
+                      self.gf('django.db.models.fields.NullBooleanField')(default=True, null=True, blank=True),
+                      keep_default=False)
+
+        # Adding field 'Location.code'
+        db.add_column('locations_location', 'code',
+                      self.gf('django.db.models.fields.CharField')(default=1, max_length=100),
+                      keep_default=False)
+
+        # Adding field 'Location.is_active'
+        db.add_column('locations_location', 'is_active',
+                      self.gf('django.db.models.fields.NullBooleanField')(default=True, null=True, blank=True),
+                      keep_default=False)
+
+        # Adding field 'Location.lft'
+        db.add_column('locations_location', 'lft',
+                      self.gf('django.db.models.fields.PositiveIntegerField')(default=1, db_index=True),
+                      keep_default=False)
+
+        # Adding field 'Location.rght'
+        db.add_column('locations_location', 'rght',
+                      self.gf('django.db.models.fields.PositiveIntegerField')(default=1, db_index=True),
+                      keep_default=False)
+
+        # Adding field 'Location.tree_id'
+        db.add_column('locations_location', 'tree_id',
+                      self.gf('django.db.models.fields.PositiveIntegerField')(default=1, db_index=True),
+                      keep_default=False)
+
+        # Adding field 'Location.level'
+        db.add_column('locations_location', 'level',
+                      self.gf('django.db.models.fields.PositiveIntegerField')(default=1, db_index=True),
+                      keep_default=False)
+
+
+    def backwards(self, orm):
+        # Deleting field 'Location.tree_parent'
+        db.delete_column('locations_location', 'tree_parent_id')
+
+        # Deleting field 'Location.name'
+        db.delete_column('locations_location', 'name')
+
+        # Deleting field 'Location.status'
+        db.delete_column('locations_location', 'status')
+
+        # Deleting field 'Location.code'
+        db.delete_column('locations_location', 'code')
+
+        # Deleting field 'Location.is_active'
+        db.delete_column('locations_location', 'is_active')
+
+        # Deleting field 'Location.lft'
+        db.delete_column('locations_location', 'lft')
+
+        # Deleting field 'Location.rght'
+        db.delete_column('locations_location', 'rght')
+
+        # Deleting field 'Location.tree_id'
+        db.delete_column('locations_location', 'tree_id')
+
+        # Deleting field 'Location.level'
+        db.delete_column('locations_location', 'level')
+
+
+    models = {
+        'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'locations.location': {
+            'Meta': {'object_name': 'Location'},
+            'code': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.NullBooleanField', [], {'default': 'True', 'null': 'True', 'blank': 'True'}),
+            'level': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
+            'lft': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'parent_id': ('django.db.models.fields.PositiveIntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'parent_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']", 'null': 'True', 'blank': 'True'}),
+            'point': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['locations.Point']", 'null': 'True', 'blank': 'True'}),
+            'rght': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
+            'status': ('django.db.models.fields.NullBooleanField', [], {'default': 'True', 'null': 'True', 'blank': 'True'}),
+            'tree_id': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
+            'tree_parent': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'children'", 'null': 'True', 'to': "orm['locations.Location']"}),
+            'type': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'locations'", 'null': 'True', 'to': "orm['locations.LocationType']"})
+        },
+        'locations.locationtype': {
+            'Meta': {'object_name': 'LocationType'},
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'unique': 'True', 'max_length': '50', 'primary_key': 'True'})
+        },
+        'locations.point': {
+            'Meta': {'object_name': 'Point'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'latitude': ('django.db.models.fields.DecimalField', [], {'max_digits': '13', 'decimal_places': '10'}),
+            'longitude': ('django.db.models.fields.DecimalField', [], {'max_digits': '13', 'decimal_places': '10'})
+        }
+    }
+
+    complete_apps = ['locations']
